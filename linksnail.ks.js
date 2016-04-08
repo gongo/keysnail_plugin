38b3481d80208dd3f28aacc8c70a820754de18b4
Toshio Ito <debug.ito@gmail.com>
2016-04-08 10:37:20 +0900

    Linksnail upgrade (#2)
    Update linksnail to 0.2.0
    
    Use "{}" in format texts. This makes the formatting more robust especially when "text" is included in the URI.
    Now formats are configurable via option linksnail.formats.
    A Function can now be used to format the link.

--- linksnail.ks.js
+++ linksnail.ks.js
@@ -5,15 +5,16 @@ var PLUGIN_INFO =
     <updateURL>https://raw.github.com/gongo/keysnail_plugin/master/linksnail.ks.js</updateURL>
     <description>Get link on the page currently open for Variety format (Markdown, org-mode, etc..)</description>
     <description lang="ja">現在開いているページへのリンクを、様々な形式 (Markdown や org-mode など)で取得します</description>
-    <version>0.1.1</version>
+    <version>0.2.0</version>
     <author mail="gonngo@gmail.com" homepage="http://d.hatena.ne.jp/gongoZ/">gongoZ</author>
     <license>MAHALO License</license>
     <license lang="ja">MAHALO ライセンス</license>
     <include>main</include>
+    <minVersion>1.8.5</minVersion>
     <detail><![CDATA[
 === Overview ===
-Get link on the page currently open for Variety format (Markdown, org-mode, etc..).
-And copies it to the clipboard.
+Get a link to the page currently open in a variety of formats (Markdown, org-mode, etc..),
+and copy it to the clipboard.
 It is also possible to get and copy links of all the tabs on current window.
 
 === Support Format ===
@@ -56,17 +57,35 @@ key.setGlobalKey(['C-c', 't'], function (ev, arg) {
     ]]></detail>
 </KeySnailPlugin>;
 
+var pOptions = plugins.setupOptions("linksnail", {
+    "formats": {
+        preset: {
+            "Markdown": "[{text}]({uri})",
+            "Org-mode": "[[{uri}][{text}]]",
+            "Textile": "\"{text}\":{uri}",
+            "reStructuredText": "`{text}` <{uri}>",
+            "HTML": "<a href=\"{uri}\">{text}</a>",
+            "Plain": "{text} / {uri}",
+            "URI": "{uri}"
+        },
+        description: M({
+            en: "Link formats object. `format` is either String or Function. If it's String it's the format template, where \"{text}\" and \"{uri}\" are replaced when linksnail is activated. If it's Function, it's called with three arguments (`text`, `uri`, `window`), and it should return the formatted link.",
+            jp: "リンクフォーマットオブジェクト。`format`はStringもしくはFunctionです。Stringの場合、これはフォーマットテンプレート文字列となります。linksnail起動時にテンプレート内の\"{text}\"と\"{uri}\"が置換されます。Functionの場合、3つの引数(`text`, `uri`, `window`)で呼び出されます。この関数はフォーマットしたリンク文字列を返す必要があります。"
+        }),
+        type: "object ({format_name: format, ... })."
+    }
+}, PLUGIN_INFO);
+
+
 var linksnail =
     (function() {
-        var formatCollection = [
-            ["Markdown"         , "[text](uri)"],
-            ["Org-mode"         , "[[uri][text]]"],
-            ["Textile"          , "\"text\":uri"],
-            ["reStructuredText" , "`text` <uri>"],
-            ["HTML"             , "<a href=\"uri\">text</a>"],
-            ["Plain"            , "text / uri"],
-            ["URI"              , "uri"],
-        ];
+        function getFormatCollection() {
+            return Object.keys(pOptions["formats"]).map(function(format_name) {
+                var general_format = pOptions["formats"][format_name];
+                var desc = _.isFunction(general_format) ? "" : general_format;
+                return [format_name, desc];
+            });
+        };
 
         function copyLink(link){
             const CLIPBOARD = Components.classes[
@@ -76,17 +95,29 @@ var linksnail =
             CLIPBOARD.copyString(link);
         };
 
-        function formatTextAndURI(format, text, uri){
-            var link = "";
-            link = format.replace("uri", uri);
-            link = link.replace("text", text);
-            return link;
+        function windowText(win) { return win.document.title; };
+        function windowURI(win) { return win.location.href; };
+
+        function getFormatter(format_name) {
+            var general_format = pOptions["formats"][format_name];
+            if(_.isFunction(general_format)) {
+                return function(win) {
+                    return general_format(windowText(win), windowURI(win), win);
+                };
+            }else {
+                return function(win) {
+                    var link = "";
+                    link = general_format.replace(/\{uri\}/g, windowURI(win));
+                    link = link.replace(/\{text\}/g, windowText(win));
+                    return link;
+                };
+            }
         };
 
         function formatSelector(next) {
             prompt.selector({
                 message    : "Select format",
-                collection : formatCollection,
+                collection : getFormatCollection(),
                 header     : ["name", "syntax"],
                 callback   : next,
             });
@@ -94,24 +125,18 @@ var linksnail =
 
         var self = {
             copyThisPage : function() {
-                formatSelector(function(index){
-                    var uri    = content.location.href;
-                    var text   = content.document.title;
-                    var format = formatCollection[index][1];
-
-                    copyLink(formatTextAndURI(format, text, uri));
+                formatSelector(function(index, collection){
+                    var formatter = getFormatter(collection[index][0]);
+                    copyLink(formatter(content));
                 });
             },
             copyAllPage : function() {
-                formatSelector(function(index){
-                    var format = formatCollection[index][1];
+                formatSelector(function(index, collection){
+                    var formatter = getFormatter(collection[index][0]);
                     var a = [(function(){
                         var browser = tab.linkedBrowser;
                         var win     = browser.contentWindow;
-
-                        var text = tab.label;
-                        var url   = win.location.href;
-                        return formatTextAndURI(format, text, url);
+                        return formatter(win);
                     })() for each (tab in Array.slice(gBrowser.mTabContainer.childNodes))];
 
                     copyLink(a.join("\n"));
